98bcde1d592af1e0c06256ceaf0b612ab9f10721
Nelson Matias <nelsonm49@yahoo.com>
2025-08-14 12:44:21 -0400

    added export as static. updated package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  reactStrictMode: false //set this to false invoke useEffect twice in developermode
+};
 export default nextConfig;
